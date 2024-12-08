dc70441f8b859181f5c93753d754b77a49add1f1
Mohibkhan2512 <mohibkp25@gmail.com>
2024-12-08 17:23:22 +0000

    from_win_08_12_24

---
+++ Trees/top_view_of_BT.cpp
@@ -0,0 +1,88 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstddef>
+#include <cstdio>
+#include <map>
+#include <queue>
+#include <utility>
+using namespace std;
+
+struct sNode {
+    int data;
+    sNode* left;
+    sNode* right;
+    sNode(int curData) {
+        data = curData;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+sNode* createTree(int arr[], int nSize) {
+    sNode* root = new sNode(arr[0]);
+    int Idx = 1;
+
+    queue<sNode*> q;
+
+    q.push(root);
+
+    while (!q.empty()) {
+        sNode* cNode = q.front();
+        q.pop();
+
+        if(Idx < nSize) {
+            cNode->left = new sNode(arr[Idx]);
+            q.push(cNode->left);
+            Idx++;
+        }
+        if(Idx < nSize) {
+            cNode->right = new sNode(arr[Idx]);
+            q.push(cNode->right);
+            Idx++;
+        }
+    }
+
+    return root;
+}
+
+void topViewOfBt(sNode* node) {
+    map<int, int> mp;
+
+    queue<pair<int, sNode*>> q;
+
+    q.push({0, node});
+
+    while (!q.empty()) {
+        sNode* cNode = q.front().second;
+        int vLevel = q.front().first;
+        q.pop();
+        if (mp.find(vLevel) == mp.end()) {
+            mp[vLevel] = cNode->data;
+        }
+
+        if (cNode->left) {
+            q.push({vLevel-1, cNode->left});
+        }
+
+        if (cNode->right) {
+            q.push({vLevel+1, cNode->right});
+        }
+    }
+    
+    cout << "Top view of BT :>> ";
+    for (auto it : mp) {
+        cout << it.second << " ";
+    }
+
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5, 6};
+    int nSize = sizeof(arr)/sizeof(arr[0]);
+
+    sNode* root = createTree(arr, nSize);
+
+    topViewOfBt(root);
+
+    printf("\n");
+}
